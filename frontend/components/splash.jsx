010019e53fe7d9e37d09963f814efd8f0996a441
Robin Wilborn <rtwilborn@gmail.com>
2017-05-15 16:47:24 -0700

    Add sign in and sign up buttons

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 
-const Splash = () => (
-  <div>
-    <h1>Welcome to Uncapp't</h1>
-    <button className="signin-button">Sign In</button>
-    <button className="signup-button">Sign up</button>
-    <video autoPlay loop id="vid" poster="/assets/beer3.png">
-      <source src="/assets/beer3.mp4" type="video/mp4" />
-      <source src="/assets/beer3.webm" type="video/webm" />
-    </video>
-  </div>
-);
+class Splash extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.handleSignIn = this.handleSignIn.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+
+  }
+
+  handleSignIn(e){
+    e.preventDefault();
+    alert("Clicked");
+  }
+
+  handleSignUp(e){
+    e.preventDefault();
+  }
+
+  render(){
+    return (
+      <div className="splash">
+        <div className="title">
+          <h1>Uncapp't</h1>
+        </div>
+
+        <div className="buttons">
+          <button className="signin-button"
+            onClick={this.handleSignIn}>Sign In</button>
+          <button className="signup-button"
+            onClick={this.handleSignUp}>Sign up</button>
+        </div>
+
+        <video autoPlay loop id="vid" poster="/assets/beer3.png">
+          <source src="/assets/beer3.mp4" type="video/mp4" />
+          <source src="/assets/beer3.webm" type="video/webm" />
+        </video>
+      </div>
+    );
+  }
+}
 
 export default Splash;
