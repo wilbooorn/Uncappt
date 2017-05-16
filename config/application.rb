3adbbed79cb19676eaa1add60d338a0eb398c64e
Robin Wilborn <rtwilborn@gmail.com>
2017-05-16 07:10:55 -0700

    Remove precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Uncappt
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
