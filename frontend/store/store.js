5602ffb9fbc945157ad08a1d185abaadfdfecec7
Robin Wilborn <rtwilborn@gmail.com>
2017-05-16 07:46:09 -0700

    Fix package.json and store for production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,11 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
   )
 );
 
