bdb1d0de3879d51093e7da59da26d5fc96b9c77d
Robin Wilborn <rtwilborn@gmail.com>
2017-05-22 16:57:34 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ user3 = User.create(username: "Marshall", password: "password", image_url: "http
 user4 = User.create(username: "Robin", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146561/robin_zbkyh7.jpg");
 user5 = User.create(username: "Barney", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495488759/barney_omjchc.jpg");
 user6 = User.create(username: "Lily", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495488862/lily_xjutgb.jpg");
-user6 = User.create(username: "Blitz", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495489037/blitz_wngim3.png");
+user7 = User.create(username: "Blitz", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495489037/blitz_wngim3.png");
 
 
 
@@ -52,17 +52,17 @@ beer9 = Beer.create(name: "Guinness", description: "Rich and creamy. Distinctive
                     brewery: "St. James' Gate Brewery", abv: 4.2, ibu: 40, style: "Stout",
                     image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495490799/guiness_esjgva.jpg")
 
-beer10 = Beer.create(name: "Angel City IPA", description: "It’s big. It’s bold. It’s got lots of character. A medium-bodied, copper-hued beer brewed with enough hops to satisfy. It’s the perfect treat for the hop inclined.",
-                    brewery: "Angel City Brewery", abv: 10, ibu: 90, style: "IPA",
-                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146803/ACB-IPA-12oz-Bottle-380x1024_qo4sh1.jpg")
+beer10 = Beer.create(name: "Aurora Hoppyalis", description: "San Diego-style IPA brewed with Simcoe, Mosaic, Amarillo, and Citra hops. Robust flavors and aromas of tropical fruit, pine, and tangerine linger through a dry, crisp finish. Tonight’s forecast: clear hoppy skies.",
+                    brewery: "Karl Strauss Brewing Company", abv: 7, ibu: 60, style: "IPA",
+                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495492891/hoppy_eh3nob.jpg")
 
-beer11 = Beer.create(name: "Angel City IPA", description: "It’s big. It’s bold. It’s got lots of character. A medium-bodied, copper-hued beer brewed with enough hops to satisfy. It’s the perfect treat for the hop inclined.",
-                    brewery: "Angel City Brewery", abv: 10, ibu: 90, style: "IPA",
-                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146803/ACB-IPA-12oz-Bottle-380x1024_qo4sh1.jpg")
+beer11 = Beer.create(name: "120 Minute IPA", description: "120 Minute IPA is continuously hopped with a copious amount of high-alpha American hops throughout the boil and whirlpool, and then dry-hopped with another pallet of hops. Unfiltered and abundently hoppy, it's the Holy Grail for hopheads!",
+                    brewery: "Dogfish Head", abv: 15, ibu: 120, style: "IPA",
+                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495493012/120_w9vsi6.jpg")
 
-beer12 = Beer.create(name: "Angel City IPA", description: "It’s big. It’s bold. It’s got lots of character. A medium-bodied, copper-hued beer brewed with enough hops to satisfy. It’s the perfect treat for the hop inclined.",
-                    brewery: "Angel City Brewery", abv: 10, ibu: 90, style: "IPA",
-                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146803/ACB-IPA-12oz-Bottle-380x1024_qo4sh1.jpg")
+beer12 = Beer.create(name: "Pliny the Elder", description: "Pliny the Elder is brewed with Amarillo, Centennial, CTZ, and Simcoe hops. It is well-balanced with malt, hops, and alcohol, slightly bitter with a fresh hop aroma of floral, citrus, and pine. Best enjoyed FRESH, which is why it is made in such limited supply.",
+                    brewery: "Russian River Brewing Company", abv: 8, ibu: 95, style: "IPA",
+                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495493268/pliny_wjdmgl.jpg")
 
 
 
@@ -81,3 +81,52 @@ checkin3 = Checkin.create(user_id: user4.id, beer_id: beer3.id, rating: 5,
 checkin4 = Checkin.create(user_id: user4.id, beer_id: beer1.id, rating: 3,
                           review: "Patrice really likes this beer. But nobody asked her!", location: "Hoser Hut",
                           image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495147054/robin_beer_dxgsg0.jpg")
+
+checkin5 = Checkin.create(user_id: user1.id, beer_id: beer4.id, rating: 4,
+                          review: "I'm just a guest, and therefore have nothing significant to say.", location: "White Castle")
+
+checkin6 = Checkin.create(user_id: user1.id, beer_id: beer9.id, rating: 3,
+                          review: "Beauty is in the eye of the beer holder", location: "Paris")
+
+checkin7 = Checkin.create(user_id: user5.id, beer_id: beer7.id, rating: 5,
+                          review: "This beer is legen... wait for it... and I hope you're not loactose intolerant because the second half of that word is DAIRY", location: "Robots vs. Wrestlers",
+                          image_url:"http://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
+
+checkin8 = Checkin.create(user_id: user6.id, beer_id: beer10.id, rating: 5,
+                          review: "Marshall bought me this. Happy wife, happy life.", location: "Art School",
+                          image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495497293/lilmarsh_ekhd6j.jpg")
+
+checkin9 = Checkin.create(user_id: user4.id, beer_id: beer11.id, rating: 4,
+                          review: "Celebrating becoming the newest currency rotation specialist.", location: "The Mall",
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495147054/robin_beer_dxgsg0.jpg")
+
+checkin10 = Checkin.create(user_id: user5.id, beer_id: beer8.id, rating: 3,
+                          review: "This beer makes me want to SUIT UP! Oh wait, I'm always suited up.", location: "Laser Tag",
+                          image_url:"http://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
+
+checkin11 = Checkin.create(user_id: user1.id, beer_id: beer6.id, rating: 5,
+                          review: "Beer is not the answer. Beer is the question, yes is the answer.", location: "Home")
+
+checkin12 = Checkin.create(user_id: user2.id, beer_id: beer4.id, rating: 4,
+                          review: "Architects don't typically drink beers on Monday, but I'll make an  exception just this once.", location: "The Vatican",
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495147130/ted_beer_cszqj1.jpg")
+
+checkin13 = Checkin.create(user_id: user2.id, beer_id: beer12.id, rating: 3,
+                          review: "This is the beer I had after I got left at the alter. I think I'm in love with it.", location: "The alter",
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495147130/ted_beer_cszqj1.jpg")
+
+checkin14 = Checkin.create(user_id: user5.id, beer_id: beer2.id, rating: 3,
+                          review: "When I'm sad, I stop being sad and be awesome instead. Then I get one of these beers.", location: "Magic Palace",
+                          image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
+
+checkin15 = Checkin.create(user_id: user4.id, beer_id: beer10.id, rating: 3,
+                          review: "\I love a Scotch that's old enough to order its own Scotch. But this beer will do for now", location: "Metro News 1",
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495147054/robin_beer_dxgsg0.jpg")
+
+checkin16 = Checkin.create(user_id: user3.id, beer_id: beer6.id, rating: 5,
+                          review: "I would walk 500 miles and I would walk 500 more. Just for this beer.", location: "The Viking's Game",
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146941/marshall_beer_txnuxq.jpg")
+
+checkin16 = Checkin.create(user_id: user7.id, beer_id: beer9.id, rating: 5,
+                          review: "AWWWW MANNNNNN", location: "an awesome place",
+                          image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495497377/05d03e9b9e8815f2ede51721ac774d61_rmryeu.jpg")
