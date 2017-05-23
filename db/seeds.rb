63fc62aed70138aa21cc4916ba5c0100427445fd
Robin Wilborn <rtwilborn@gmail.com>
2017-05-22 17:53:11 -0700

    Change http images to https in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ user1 = User.create(username: "Guest", password: "password", image_url: "https:/
 user2 = User.create(username: "Ted", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146369/ted_zmpiyd.jpg");
 user3 = User.create(username: "Marshall", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146373/marshall_k992zf.jpg");
 user4 = User.create(username: "Robin", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495146561/robin_zbkyh7.jpg");
-user5 = User.create(username: "Barney", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495488759/barney_omjchc.jpg");
-user6 = User.create(username: "Lily", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495488862/lily_xjutgb.jpg");
-user7 = User.create(username: "Blitz", password: "password", image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495489037/blitz_wngim3.png");
+user5 = User.create(username: "Barney", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495488759/barney_omjchc.jpg");
+user6 = User.create(username: "Lily", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495488862/lily_xjutgb.jpg");
+user7 = User.create(username: "Blitz", password: "password", image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495489037/blitz_wngim3.png");
 
 
 
@@ -30,39 +30,39 @@ beer3 = Beer.create(name: "Angel City IPA", description: "It’s big. It’s bol
 
 beer4 = Beer.create(name: "Racer 5", description: "This hoppy IPA is a full bodied beer brewed with malted barley, wheat, and crystal malts. The malt base is designed to highlight the unique floral qualities Columbus and Cascade hops from the Pacific Northwest.",
                     brewery: "Bear Republic", abv: 7.5, ibu: 75, style: "IPA",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495489360/racer5_dvczb3.jpg")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495489360/racer5_dvczb3.jpg")
 
 beer5 = Beer.create(name: "Corona Extra", description: "With a refreshing, smooth taste balanced between heavier European imports and lighter domestic beer, Corona is an even-keeled cerveza with fruity-honey aromas and a touch of malt. The flavor is crisp, clean and well balanced between hops and malt.",
                     brewery: "Cervecería Modelo", abv: 4.6, ibu: 25, style: "Lager",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495489588/Corona_oskoea.jpg")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495489588/Corona_oskoea.jpg")
 
 beer6 = Beer.create(name: "PILS", description: "Brewed with loads of imported Saaz hops and a bottom-fermenting yeast strain that leaves it Light and Crisp and Easy to Slam, yet full of real flavor and all the things you yearn for.",
                     brewery: "Lagunitas", abv: 10, ibu: 90, style: "Pilsner",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495490063/PILS_foh9aq.png")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495490063/PILS_foh9aq.png")
 
 beer7 = Beer.create(name: "Hop Stoopid", description: "The beer with a guarantee built right into the name, this one is destined to blast through the flavors from the night before. Massively dry-hopped for your aromatic pleasure.",
                     brewery: "Lagunitas", abv: 8, ibu: 102, style: "Imperial IPA",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495490246/Hop-Stoopid-12oz_tzbulq.png")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495490246/Hop-Stoopid-12oz_tzbulq.png")
 
 beer8 = Beer.create(name: "Black Marlin", description: "Crafted it from a unique blend of roasted and crystallized malts that give it a smooth, chocolaty, caramel profile. Then toss in a healthy dose of American hops to serve up full-bodied flavor worthy of its San Diego roots.",
                     brewery: "Ballast Point", abv: 6, ibu: 46, style: "Porter",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495490494/black_marlin_wiucam.png")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495490494/black_marlin_wiucam.png")
 
 beer9 = Beer.create(name: "Guinness", description: "Rich and creamy. Distinctively black. Velvety in its finish. This iconic beer is defined by harmony. Sip after sip, sweet counters bitter as the malt arrives on cue to compliment a base of roasted barley.",
                     brewery: "St. James' Gate Brewery", abv: 4.2, ibu: 40, style: "Stout",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495490799/guiness_esjgva.jpg")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495490799/guiness_esjgva.jpg")
 
 beer10 = Beer.create(name: "Aurora Hoppyalis", description: "San Diego-style IPA brewed with Simcoe, Mosaic, Amarillo, and Citra hops. Robust flavors and aromas of tropical fruit, pine, and tangerine linger through a dry, crisp finish. Tonight’s forecast: clear hoppy skies.",
                     brewery: "Karl Strauss Brewing Company", abv: 7, ibu: 60, style: "IPA",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495492891/hoppy_eh3nob.jpg")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495492891/hoppy_eh3nob.jpg")
 
 beer11 = Beer.create(name: "120 Minute IPA", description: "120 Minute IPA is continuously hopped with a copious amount of high-alpha American hops throughout the boil and whirlpool, and then dry-hopped with another pallet of hops. Unfiltered and abundently hoppy, it's the Holy Grail for hopheads!",
                     brewery: "Dogfish Head", abv: 15, ibu: 120, style: "IPA",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495493012/120_w9vsi6.jpg")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495493012/120_w9vsi6.jpg")
 
 beer12 = Beer.create(name: "Pliny the Elder", description: "Pliny the Elder is brewed with Amarillo, Centennial, CTZ, and Simcoe hops. It is well-balanced with malt, hops, and alcohol, slightly bitter with a fresh hop aroma of floral, citrus, and pine. Best enjoyed FRESH, which is why it is made in such limited supply.",
                     brewery: "Russian River Brewing Company", abv: 8, ibu: 95, style: "IPA",
-                    image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495493268/pliny_wjdmgl.jpg")
+                    image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495493268/pliny_wjdmgl.jpg")
 
 
 
@@ -90,11 +90,11 @@ checkin6 = Checkin.create(user_id: user1.id, beer_id: beer9.id, rating: 3,
 
 checkin7 = Checkin.create(user_id: user5.id, beer_id: beer7.id, rating: 5,
                           review: "This beer is legen... wait for it... and I hope you're not lactose intolerant because the second half of that word is DAIRY", location: "Robots vs. Wrestlers",
-                          image_url:"http://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
+                          image_url:"https://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
 
 checkin8 = Checkin.create(user_id: user6.id, beer_id: beer10.id, rating: 5,
                           review: "Marshall bought me this. Happy wife, happy life.", location: "Art School",
-                          image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495497293/lilmarsh_ekhd6j.jpg")
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495497293/lilmarsh_ekhd6j.jpg")
 
 checkin9 = Checkin.create(user_id: user4.id, beer_id: beer11.id, rating: 4,
                           review: "Celebrating becoming the newest currency rotation specialist.", location: "The Mall",
@@ -102,7 +102,7 @@ checkin9 = Checkin.create(user_id: user4.id, beer_id: beer11.id, rating: 4,
 
 checkin10 = Checkin.create(user_id: user5.id, beer_id: beer8.id, rating: 3,
                           review: "This beer makes me want to SUIT UP! Oh wait, I'm always suited up.", location: "Laser Tag",
-                          image_url:"http://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
+                          image_url:"https://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
 
 checkin11 = Checkin.create(user_id: user1.id, beer_id: beer6.id, rating: 5,
                           review: "Beer is not the answer. Beer is the question, yes is the answer.", location: "Home")
@@ -117,7 +117,7 @@ checkin13 = Checkin.create(user_id: user2.id, beer_id: beer12.id, rating: 3,
 
 checkin14 = Checkin.create(user_id: user5.id, beer_id: beer2.id, rating: 3,
                           review: "When I'm sad, I stop being sad and be awesome instead. Then I get one of these beers.", location: "Magic Palace",
-                          image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495497117/barney-beer_zondws.jpg")
 
 checkin15 = Checkin.create(user_id: user4.id, beer_id: beer10.id, rating: 3,
                           review: "\I love a Scotch that's old enough to order its own Scotch. But this beer will do for now", location: "Metro News 1",
@@ -129,4 +129,4 @@ checkin16 = Checkin.create(user_id: user3.id, beer_id: beer6.id, rating: 5,
 
 checkin16 = Checkin.create(user_id: user7.id, beer_id: beer9.id, rating: 5,
                           review: "AWWWW MANNNNNN", location: "an awesome place",
-                          image_url: "http://res.cloudinary.com/dslok1mwv/image/upload/v1495497377/05d03e9b9e8815f2ede51721ac774d61_rmryeu.jpg")
+                          image_url: "https://res.cloudinary.com/dslok1mwv/image/upload/v1495497377/05d03e9b9e8815f2ede51721ac774d61_rmryeu.jpg")
