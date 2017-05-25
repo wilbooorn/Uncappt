3a0ccb5038bf897ba56b4fd741a054d09b6a0893
Robin Wilborn <rtwilborn@gmail.com>
2017-05-24 17:01:40 -0700

    Render beers alphabetically

--- frontend/components/beer/beers_index.jsx
+++ frontend/components/beer/beers_index.jsx
@@ -36,7 +36,23 @@ class BeersIndex extends React.Component {
         </div>
       );
     }
-    return <div></div>;
+    else {
+      return (
+        <div className="no-beers-index-container">
+          <NavBarContainer page="index" history={this.props.history}/>
+          <ul className="all-beers">
+            <div className="all-the-beers">
+              <h1>All Beers</h1>
+            </div>
+            <div className="add-beer-show">
+              <p className="dif-member">Can't find what you're looking for?</p>
+              <Link className="link" to="/beers/new">Add Your Favorite Beer</Link>
+            </div>
+          </ul>
+          <Footer />
+        </div>
+      );
+    }
   }
 }
 
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,5 +1,15 @@
 export const selectAllBeers = state => {
-  return Object.keys(state.beerInfo.beers).map(beerId => state.beerInfo.beers[beerId]);
+  return Object.keys(state.beerInfo.beers)
+  .map(beerId => state.beerInfo.beers[beerId])
+  .sort((a, b) => {
+    if (a.name < b.name){
+      return -1;
+    }else if (a.name > b.name){
+      return 1;
+    }else{
+      return 0;
+    }
+  });
 };
 
 export const selectAllCheckins = state => {
